29b3b0f619cc5e61c607e752516dc47f48e2c566
Chris Dent <cdent@anticdent.org>
2019-02-25 22:00:42 +0000

    Remove redundant second cast to int
    In commit d51dcaa87c416ea0a69074d2ad4de26d72bbefa1 the 'used'
    value was explicitly made an int, twice. It only needs to be
    done once.
    
    Change-Id: Ib9dc6ba004ffacda9044f42ace745d147d294ca0

--- placement/objects/resource_provider.py
+++ placement/objects/resource_provider.py
@@ -3496,8 +3496,7 @@ def _build_provider_summaries(context, usages, prov_traits):
         rpsr = ProviderSummaryResource(
             resource_class=rc_name,
             capacity=cap,
-            # FIXME(cdent): This might be a decimal
-            used=int(used),
+            used=used,
             max_unit=usage['max_unit'],
         )
         summary.resources.append(rpsr)
