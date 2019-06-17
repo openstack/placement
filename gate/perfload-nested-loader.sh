8723bd7772f2b90deceaf0887951f90d0dc0346d
Chris Dent <cdent@anticdent.org>
2019-06-17 15:47:36 +0100

    Nested provider performance testing
    This change duplicates the ideas started in with the placement-perfload
    job and builds on it to create a set of nested trees that can be
    exercised.
    
    In placement-perfload, placeload is used to create the providers. This
    proves to be cumbersome for nested topologies so this change starts
    a new model: Using parallel [1] plus instrumented gabbi to create
    nested topologies in a declarative fashion.
    
    gate/perfload-server.sh sets up placement db and starts a uwsgi server.
    
    gate/perfload-nested-loader.sh is called in the playbook to cause gabbi
    to create the nested topology described in
    gate/gabbits/nested-perfload.yaml. That topology is intentionally very
    naive right now but should be made more realisitc as we continue to
    develop nested features.
    
    There's some duplication between perfload.yaml and
    nested-perfload.yaml that will be cleared up in a followup.
    
    [1] https://www.gnu.org/software/parallel/ (although the version on
    ubuntu is a non-GPL clone)
    
    Story: 2005443
    Task: 30487
    Change-Id: I617161fde5b844d7f52dc766f85c1b9f1b139e4a

---
+++ gate/perfload-nested-loader.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -a
+HOST=$1
+GABBIT=$2
+
+# By default the placement server is set up with noauth2 authentication
+# handling. If that is changed to keystone, a $TOKEN can be generated in
+# the calling environment and used instead of the default 'admin'.
+TOKEN=${TOKEN:-admin}
+
+# These are the dynamic/unique values for individual resource providers
+# that need to be set for each run a gabbi file. Values that are the same
+# for all the resource providers (for example, traits and inventory) should
+# be set in $GABBIT.
+CN_UUID=$(uuidgen)
+N1_UUID=$(uuidgen)
+N2_UUID=$(uuidgen)
+
+# Run gabbi silently.
+gabbi-run -q $HOST -- $GABBIT
