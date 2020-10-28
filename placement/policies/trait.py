1f9529f8a0148b271d90141dd6ee30203730878c
Lance Bragstad <lbragstad@gmail.com>
2020-10-28 21:06:15 +0000

    Implement secure RBAC for traits
    This commit updates the policies for traits in placement to support read-only
    roles.
    
    This is part of a broader community effort to support read-only roles
    and implement secure, consistent default policies.
    
    Change-Id: I5aec5f0ad24acd2c14c85a0a878c50790c2ab415

--- placement/policies/trait.py
+++ placement/policies/trait.py
@@ -11,6 +11,7 @@
 #    under the License.
 
 
+from oslo_log import versionutils
 from oslo_policy import policy
 
 from placement.policies import base
@@ -27,91 +28,145 @@ TRAITS_SHOW = TRAITS_PREFIX % 'show'
 TRAITS_UPDATE = TRAITS_PREFIX % 'update'
 TRAITS_DELETE = TRAITS_PREFIX % 'delete'
 
+DEPRECATED_REASON = """
+The traits API now supports a read-only role by default.
+"""
+
+deprecated_list_traits = policy.DeprecatedRule(
+    name=TRAITS_LIST,
+    check_str=base.RULE_ADMIN_API
+)
+deprecated_show_trait = policy.DeprecatedRule(
+    name=TRAITS_SHOW,
+    check_str=base.RULE_ADMIN_API
+)
+deprecated_rp_traits_list = policy.DeprecatedRule(
+    name=RP_TRAIT_LIST,
+    check_str=base.RULE_ADMIN_API
+)
+deprecated_traits_update = policy.DeprecatedRule(
+    name=TRAITS_UPDATE,
+    check_str=base.RULE_ADMIN_API
+)
+deprecated_traits_delete = policy.DeprecatedRule(
+    name=TRAITS_DELETE,
+    check_str=base.RULE_ADMIN_API
+)
+deprecated_rp_trait_update = policy.DeprecatedRule(
+    name=RP_TRAIT_UPDATE,
+    check_str=base.RULE_ADMIN_API
+)
+deprecated_rp_trait_delete = policy.DeprecatedRule(
+    name=RP_TRAIT_DELETE,
+    check_str=base.RULE_ADMIN_API
+)
+
 
 rules = [
     policy.DocumentedRuleDefault(
-        TRAITS_LIST,
-        base.RULE_ADMIN_API,
-        "List traits.",
-        [
+        name=TRAITS_LIST,
+        check_str=base.SYSTEM_READER,
+        description="List traits.",
+        operations=[
             {
                 'method': 'GET',
                 'path': '/traits'
             }
         ],
-        scope_types=['system']
+        scope_types=['system'],
+        deprecated_rule=deprecated_list_traits,
+        deprecated_reason=DEPRECATED_REASON,
+        deprecated_since=versionutils.deprecated.WALLABY
     ),
     policy.DocumentedRuleDefault(
-        TRAITS_SHOW,
-        base.RULE_ADMIN_API,
-        "Show trait.",
-        [
+        name=TRAITS_SHOW,
+        check_str=base.SYSTEM_READER,
+        description="Show trait.",
+        operations=[
             {
                 'method': 'GET',
                 'path': '/traits/{name}'
             }
         ],
         scope_types=['system'],
+        deprecated_rule=deprecated_show_trait,
+        deprecated_reason=DEPRECATED_REASON,
+        deprecated_since=versionutils.deprecated.WALLABY
     ),
     policy.DocumentedRuleDefault(
-        TRAITS_UPDATE,
-        base.RULE_ADMIN_API,
-        "Update trait.",
-        [
+        name=TRAITS_UPDATE,
+        check_str=base.SYSTEM_ADMIN,
+        description="Update trait.",
+        operations=[
             {
                 'method': 'PUT',
                 'path': '/traits/{name}'
             }
         ],
         scope_types=['system'],
+        deprecated_rule=deprecated_traits_update,
+        deprecated_reason=DEPRECATED_REASON,
+        deprecated_since=versionutils.deprecated.WALLABY
     ),
     policy.DocumentedRuleDefault(
-        TRAITS_DELETE,
-        base.RULE_ADMIN_API,
-        "Delete trait.",
-        [
+        name=TRAITS_DELETE,
+        check_str=base.SYSTEM_ADMIN,
+        description="Delete trait.",
+        operations=[
             {
                 'method': 'DELETE',
                 'path': '/traits/{name}'
             }
         ],
         scope_types=['system'],
+        deprecated_rule=deprecated_traits_delete,
+        deprecated_reason=DEPRECATED_REASON,
+        deprecated_since=versionutils.deprecated.WALLABY
     ),
     policy.DocumentedRuleDefault(
-        RP_TRAIT_LIST,
-        base.RULE_ADMIN_API,
-        "List resource provider traits.",
-        [
+        name=RP_TRAIT_LIST,
+        check_str=base.SYSTEM_READER,
+        description="List resource provider traits.",
+        operations=[
             {
                 'method': 'GET',
                 'path': '/resource_providers/{uuid}/traits'
             }
         ],
         scope_types=['system'],
+        deprecated_rule=deprecated_rp_traits_list,
+        deprecated_reason=DEPRECATED_REASON,
+        deprecated_since=versionutils.deprecated.WALLABY
     ),
     policy.DocumentedRuleDefault(
-        RP_TRAIT_UPDATE,
-        base.RULE_ADMIN_API,
-        "Update resource provider traits.",
-        [
+        name=RP_TRAIT_UPDATE,
+        check_str=base.SYSTEM_ADMIN,
+        description="Update resource provider traits.",
+        operations=[
             {
                 'method': 'PUT',
                 'path': '/resource_providers/{uuid}/traits'
             }
         ],
         scope_types=['system'],
+        deprecated_rule=deprecated_rp_trait_update,
+        deprecated_reason=DEPRECATED_REASON,
+        deprecated_since=versionutils.deprecated.WALLABY
     ),
     policy.DocumentedRuleDefault(
-        RP_TRAIT_DELETE,
-        base.RULE_ADMIN_API,
-        "Delete resource provider traits.",
-        [
+        name=RP_TRAIT_DELETE,
+        check_str=base.SYSTEM_ADMIN,
+        description="Delete resource provider traits.",
+        operations=[
             {
                 'method': 'DELETE',
                 'path': '/resource_providers/{uuid}/traits'
             }
         ],
         scope_types=['system'],
+        deprecated_rule=deprecated_rp_trait_delete,
+        deprecated_reason=DEPRECATED_REASON,
+        deprecated_since=versionutils.deprecated.WALLABY
     ),
 ]
 
